205ca82ebe1b0b4a7e1a76231fc819e04904ade0
Tiit Papp <tiit.papp@gmail.com>
2025-07-03 18:31:50 +0300

    Update bootstrap.cfg.php

--- install/project/includes/configuration/active/bootstrap.cfg.php
+++ install/project/includes/configuration/active/bootstrap.cfg.php
@@ -7,7 +7,7 @@
 // This points to the bootstrap CDN by default. You can point it to a local copy if you want.
 //define('QCUBED_BOOTSTRAP_CSS', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
 //define('QCUBED_BOOTSTRAP_JS', "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js");
-define('QCUBED_BOOTSTRAP_ASSETS_URL', dirname(QCUBED_BASE_URL) . '/qcubed-4/bootstrap/assets');
+define('QCUBED_BOOTSTRAP_ASSETS_URL', dirname(QCUBED_BASE_URL) . '/qcubed-4/plugin-bootstrap/assets');
 
 define('QCUBED_BOOTSTRAP_CSS', QCUBED_URL_PREFIX . '/project/assets/bootstrap/css/bootstrap.min.css');
 define('QCUBED_BOOTSTRAP_JS', QCUBED_URL_PREFIX . '/project/assets/bootstrap/js/bootstrap.min.js');
